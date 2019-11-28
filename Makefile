c095b73a97886ae172dd1bbafdd41f5f9012d60e
Nicholas Kosa <nkosa427@gmail.com>
2019-11-28 13:31:30 -0800

    Basic server constructed. Still need to test it.

--- Makefile
+++ Makefile
@@ -3,15 +3,30 @@ CC=gcc -std=c99
 all:
 
 k:
+	rm -f keygen
 	$(CC) keygen.c -o keygen
 
-r:
-	$(CC) smallsh.c -o smallsh -D_XOPEN_SOURCE
+ed:
+	rm -f ed
+	$(CC) otp_enc_d.c -o ed
 
-c:
-	./smallsh
+ec:
+	rm -f ec
+	$(CC) otp_enc.c -o ec
+
+dd:
+	rm -f dd
+	$(CC) otp_dec_d.c -o dd
+
+dc:
+	rm -f dc
+	$(CC) otp_dec.c -o dc
 
 clean:
-	rm -f smallsh
+	rm -f keygen
+	rm -f ec
+	rm -f ed
+	rm -f dc
+	rm -f dd
 
 
--- otp_enc_d.c
+++ otp_enc_d.c
@@ -1,9 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char* argv[])
 {
+	int listenFD, bindFD, estFD;
+	int port;
+	int charsRead, charsSent;
+
+	socklen_t clientInfo;
+	char str[256];
+	struct sockaddr_in serverAddr;
+	struct sockaddr_in clientAddr;
+
+	if(argc < 2){
+		fprintf(stderr, "Enter correct number of args. \n");
+		exit(1);
+	}
+
+	memset((char*)&serverAddr, '\0', sizeof(serverAddr));
+	port = atoi(argv[1]);
 	
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+	listenFD = socket(AF_INET, SOCK_STREAM, 0);
+	if(listenFD < 0){
+		fprintf(stderr, "Socket open error\n");
+	}
+
+	bindFD = bind(listenFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(bindFD < 0){
+		fprintf(stderr, "Binding error\n");
+	}
+
+	listen(listenFD, 5);
+
+	clientInfo = sizeof(clientAddr);
+	estFD = accept(listenFD, (struct sockaddr*)&clientAddr, &clientInfo);
+	if(estFD < 0){
+		fprintf(stderr, "Accept error\n");
+	}
+
+	memset(str, '\0', 256);
+	charsRead = recv(estFD, str, 255, 0);
+	if(charsRead < 0){
+		fprintf(stderr, "Reading error\n");
+	}
+
+	printf("Server received message from client:\t");
+	printf("%s\n", str);
+
+	charsSent = send(estFD, "Message received\n", 17, 0);
+	if(charsSent < 0){
+		fprintf(stderr, "Error sending to client\n");
+	}
+
+	close(estFD);
+	close(listenFD);
+
 	return 0;
 }
